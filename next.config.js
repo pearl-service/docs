ea53ce10dc3db7fee294073ec17c5bc69d048b91
sxck1337 <sxckv3@gmail.com>
2023-12-02 21:41:25 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
+// next.config.js
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
-  images: {
-    unoptimized: true,
-  }
 })
 
-module.exports = withNextra()
+const withImages = require('next-images');
 
+module.exports = withNextra(withImages({
+  images: {
+    unoptimized: true, 
+  },
+}));
