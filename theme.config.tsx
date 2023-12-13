2a798cacb2d9a11ba52b9e461b1af8e0fe0c9bc6
sxck1337 <sxckv3@gmail.com>
2023-12-13 21:43:13 +0100

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -9,6 +9,13 @@ const config: DocsThemeConfig = {
   footer: {
     text: 'Pearl-Service.best - 2023',
   },
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:title" content="Pearl-Service" />
+      <meta property="og:description" content="Documentation center for products we provide" />
+    </>
+  ),
   useNextSeoProps() {
     return {
       titleTemplate: '%s – Pearl-Service',
